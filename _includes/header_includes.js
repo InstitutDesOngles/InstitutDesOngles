69237686c603af5e3b70338f45e8c60d2f9bc43d
EVS <vinas.emmanuel@gmail.com>
2016-03-01 00:12:41 +0100

    Updated menu

--- _includes/header_includes.js
+++ _includes/header_includes.js
@@ -1,4 +1,4 @@
-{% if page.group == 5 %}
+{% if page.map == true %}
   <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAnjvUQmcVvgzwdvdtAvz_IuuGbmRsZO54"></script>
   <script type="text/javascript" src="{{site.baseurl}}/public/js/idomap.js"></script>
 {% endif %}
